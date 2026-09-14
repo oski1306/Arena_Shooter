c861e8761d16281f6466a4cc99da29960b00738f
oski1306 <124702797+oski1306@users.noreply.github.com>
2026-09-14 16:57:24 +0200

    Add sprint boost pickup and green material 14.09.26
    Add a green material and a pickup GameObject to SampleScene, plus logic to handle sprint boosts. Files added: Assets/Materials/green.mat and its .meta. SampleScene.unity updated to include a Cube with MeshRenderer (using the new green material), BoxCollider (trigger) and the SprintBoost component. SprintBoost.cs updated to cache MeshRenderer and BoxCollider in Start, disable them when the player triggers the pickup, set GameManager.sprintBoost, start the boost coroutine, and destroy the pickup after 15s.

--- Assets/Scripts/SprintBoost.cs
+++ Assets/Scripts/SprintBoost.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class SprintBoost : MonoBehaviour
 {
+    private MeshRenderer meshRenderer;
+    private BoxCollider boxCollider;
+
+    void Start()
+    {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        boxCollider = gameObject.GetComponent<BoxCollider>();
+    }
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             GameManager.sprintBoost = true;
             StartCoroutine(SpeedBoostCountdown());
+
+            boxCollider.enabled = false;
+            meshRenderer.enabled = false;
         }
     }
 
@@ -16,5 +28,6 @@ public class SprintBoost : MonoBehaviour
     {
         yield return new WaitForSeconds(15f);
         GameManager.sprintBoost = false;
+        Destroy(gameObject);
     }
 }
